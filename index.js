4417b27954c8ea0471d66e3c04c0441cf23af795
Laurence Burt <laurencemb@hotmail.co.uk>
2023-03-06 09:33:53 +0000

    updated various employye type files to create class contructors

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+const Employee = require("./lib/Employee")
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -9,6 +11,8 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+
+const e = new Employee("Alice", 100, "test@test.com" )
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
